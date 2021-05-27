f0061a398ee73e152c07dc370a0e3058cd6900a8
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-05-28 01:30:20 +0900

    각도 계산 함수 + 어깨, 팔꿈치, 손목 좌표값 저장 및 출력

--- exercise_count.py
+++ exercise_count.py
@@ -1,8 +1,22 @@
 import cv2   # OpenCV
 import mediapipe as mp   # Mediapipe
+import numpy as np   # 수치 연산을 위한 Numpy 사용
 
 mp_drawing = mp.solutions.drawing_utils   # 포즈 랜드마크를 그릴 수 있게 도와주는 유틸리티
 mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])   # 세 점의 각도(radian)를 np.arctan2(y,x)를 이용해 계산
+    angle = np.abs(radians*180.0/np.pi)   # angle 계산
+    
+    if angle >180.0:
+        angle = 360-angle   # 각도의 최대값 180도
+        
+    return angle   # angle 값 반환
 
 cap = cv2.VideoCapture(0)   # OpenCV의 VideoCapture를 이용해 웹캠 사용
 with mp_pose.Pose(   
@@ -20,10 +34,32 @@ with mp_pose.Pose(
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)   # 다시 RGB를 BGR로 변환
 
+
+    try:
+        landmarks = results.pose_landmarks.landmark   # 랜드마크 좌표 추출
+            
+        # 각각의 좌표 저장
+        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]   # 왼쪽 어깨 좌표(x,y) 저장
+        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]   # 왼쪽 팔꿈치 좌표(x,y) 저장
+        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]   # 왼쪽 손목 좌표(x,y) 저장
+        
+        # 각도 계산
+        left_arm_angle = calculate_angle(shoulder, elbow, wrist)   # 왼쪽 어깨, 팔꿈치, 손목의 각도 계산
+        
+        # 각도 커스텀 출력
+        cv2.putText(image, str(left_arm_angle),
+            tuple(np.multiply(elbow, [640, 480]).astype(int)),   # 팔꿈치 부분에 Text 출력
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+        )
+                       
+    except:
+        pass
+
     mp_drawing.draw_landmarks(   # 랜드마크 드로잉
         image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
     cv2.imshow('Exercise Count', image)   # OpenCV imshow를 이용해 결과물 출력
     if cv2.waitKey(5) & 0xFF == ord('q'):   # q 누르면 웹캠 종료
-      break
+        break
 cap.release()
+

6f7a82bd3db840a661a0c752b1d6ac8889ec6ff1
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-05-28 03:13:22 +0900

    오른팔 덤벨 컬, 스쿼트 카운트 출력 추가

--- exercise_count.py
+++ exercise_count.py
@@ -22,8 +22,12 @@ def calculate_angle(a,b,c):
 ### Mediapipe Solution ###
 cap = cv2.VideoCapture(0)   # OpenCV의 VideoCapture를 이용해 웹캠 사용
 
-count = 0   # 카운트 초기화
-state = None   # 상태 초기화
+left_count = 0   # 카운트 초기화
+right_count = 0   
+squat_count = 0
+left_state = None   # 상태 초기화
+right_state = None
+squat_state = None
 
 with mp_pose.Pose(   
     min_detection_confidence=0.5,
@@ -45,26 +49,25 @@ with mp_pose.Pose(
         landmarks = results.pose_landmarks.landmark   # 랜드마크 좌표 추출
             
         ### 각각의 좌표 저장 ### (거울모드이므로 반대로 저장)
-        # 왼팔
-        left_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]   # 왼쪽 어깨 좌표(x,y) 저장
-        left_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]   # 왼쪽 팔꿈치 좌표(x,y) 저장
-        left_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]   # 왼쪽 손목 좌표(x,y) 저장
-        
         # 오른팔
         right_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]   # 오른쪽 어깨 좌표(x,y) 저장
         right_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]   # 오른쪽 팔꿈치 좌표(x,y) 저장
         right_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]   # 오른쪽 손목 좌표(x,y) 저장
 
-        # 왼쪽 다리
-        left_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]   # 왼쪽 힙 좌표(x,y) 저장
-        left_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]   # 왼쪽 무릎 좌표(x,y) 저장
-        left_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]   # 왼쪽 발목 좌표(x,y) 저장
-
+        # 왼팔
+        left_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]   # 왼쪽 어깨 좌표(x,y) 저장
+        left_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]   # 왼쪽 팔꿈치 좌표(x,y) 저장
+        left_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]   # 왼쪽 손목 좌표(x,y) 저장
+        
+        # 다리
+        hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]   # 힙 좌표(x,y) 저장
+        knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]   # 무릎 좌표(x,y) 저장
+        ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]   # 발목 좌표(x,y) 저장
 
         ### 각도 계산 ###
         left_arm_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)   # 왼쪽 어깨, 팔꿈치, 손목의 각도 계산
         right_arm_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)   # 오른쪽 어깨, 팔꿈치, 손목의 각도 계산
-        left_leg_angle = calculate_angle(left_hip, left_knee, left_ankle)   # 왼쪽 힙, 무릎, 발목의 각도 계산
+        leg_angle = calculate_angle(hip, knee, ankle)   # 힙, 무릎, 발목의 각도 계산
 
         ### 각도 커스텀 출력 ###
         # 왼팔
@@ -79,36 +82,79 @@ with mp_pose.Pose(
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
         )
 
-        # 왼쪽 다리
-        cv2.putText(image, str(left_leg_angle),
-            tuple(np.multiply(left_knee, [640, 480]).astype(int)),   # 무릎 부분에 Text 출력
+        # 다리
+        cv2.putText(image, str(leg_angle),
+            tuple(np.multiply(knee, [640, 480]).astype(int)),   # 무릎 부분에 Text 출력
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
         )
 
         # 왼팔 덤벨 컬 카운트
         if left_arm_angle > 160:   # 왼팔 각도가 160보다 크면(팔을 펴면),
-            state = "down"   # down
-        if left_arm_angle < 30 and state == 'down':   # 각도가 30보다 작고 state가 down이면(팔을 굽히면),
-            state = "up"   # up
-            count += 1   # count + 1
-            print(count)
+            left_state = "down"   # down
+        if left_arm_angle < 30 and left_state == 'down':   # 각도가 30보다 작고 state가 down이면(팔을 굽히면),
+            left_state = "up"   # up
+            left_count += 1   # count + 1
+            print(left_count)
+
+        # 오른팔 덤벨 컬 카운트
+        if right_arm_angle > 160:   # 오른팔 각도가 160보다 크면(팔을 펴면),
+            right_state = "down"   # down
+        if right_arm_angle < 30 and right_state == 'down':   # 각도가 30보다 작고 state가 down이면(팔을 굽히면),
+            right_state = "up"   # up
+            right_count += 1   # count + 1
+            print(right_count)
+
+        # 스쿼트 카운트
+        if leg_angle < 140:   # 각도가 140보다 작으면(다리를 굽히면),
+            squat_state = "down"   # down
+        if leg_angle > 160 and squat_state == 'down':   # 다리 각도가 170보다 크고, state가 down이면(다리을 펴면)
+            squat_state = "up"   # up
+            squat_count += 1   # count + 1
+            print(squat_count)
+        
 
     except:
         pass
         
     ### 출력 ###
     cv2.putText(image, 'Left', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+
+    cv2.putText(image, 'Right', (490,12), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+
+    cv2.putText(image, 'Squat', (490,440), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
 
     # 왼팔 덤벨 컬 카운트 출력
-    cv2.putText(image, str(count), 
-                    (10,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+    cv2.putText(image, str(left_count), 
+                (10,40), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
         
     # 왼팔 덤벨 컬 상태 출력
-    cv2.putText(image, state, 
-                    (60,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+    cv2.putText(image, left_state, 
+                (60,40), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+
+    # 오른팔 덤벨 컬 카운트 출력
+    cv2.putText(image, str(right_count), 
+                (490,40), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+        
+    # 오른팔 덤벨 컬 상태 출력
+    cv2.putText(image, right_state, 
+                (540,40), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+
+    # 스쿼트 카운트 출력
+    cv2.putText(image, str(squat_count), 
+                (490,468), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+        
+    # 스쿼트 상태 출력
+    cv2.putText(image, squat_state, 
+                (540,468), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
 
     ### 랜드마크 드로잉 ###
     mp_drawing.draw_landmarks(   
